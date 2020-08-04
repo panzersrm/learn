609ae4adc3ff9074ff251f24b20190a5333304de
panzersrm <37176781+panzersrm@users.noreply.github.com>
2020-08-04 13:49:50 +0200

    repaired algo

--- sum-all-odd-fibonacci-numbers.js
+++ sum-all-odd-fibonacci-numbers.js
@@ -1,8 +1,32 @@
-function isPrime(x) {
-  for (let i = 2; i < x; i++) {
-    if (x % i === 0) return false;
+function sumFibs(num) {
+  let fibS = [1,1];
+  for (let i=0; i<num; i++){
+    fibS.push(fibS[i]+fibS[i+1])
   }
-  return x !== 1 && x !== 0;
+  let fib = fibS.filter(el=>el<=num);
+  return fib.reduce((ac,el)=>el%2===1?ac+=el:ac,0);
+
+  /*more simple to understand reduce function:
+  return fib.reduce((ac,el)=>{
+    if (el%2===1){
+      ac+=el;
+    } 
+    return ac
+  },0);
+  */
 }
-//test here
-console.log(isPrime(10));
+
+console.log(sumFibs(4));
+
+
+//Own solution after reading FCC solutions:
+/*
+function sumFibs(num) {
+  let fibS = [1,1];
+  for (let i=0; i<num; i++){
+    fibS.push(fibS[i]+fibS[i+1])
+  }
+  let fib = fibS.filter(el=>el<=num && el%2==1);
+  return fib.reduce((ac,el)=>ac+=el,0);
+}
+*/

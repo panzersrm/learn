4fedaf64929620278a28c47fb0ca78ec96d411ab
panzersrm <37176781+panzersrm@users.noreply.github.com>
2020-11-22 00:19:59 +0100

    Update convert-html-entities.js

--- convert-html-entities.js
+++ convert-html-entities.js
@@ -8,8 +8,17 @@ function convertHTML(str) {
     '\'' : '&apos;'
   };
   
+  //new solution:
+        //create an array a with all characters in the string
+    //let a = str.split('');
+        //create an array ar by mapping each element in array a: if escapeChars[el] exists retrurn escapeChars[el], if not return el
+    //let ar = a.map(el => escapeChars[el]? escapeChars[el] : el)
+        //join all elements of ar for result
+    //return ar.join("");
+  
   let regex = /[^\w\s]/g;
   let matched = str.match(regex);
+  
   
   //FCC solution... I was close to this but instead of writing a function as replace() second argument, I was trying to get directly the values out of the escapeChars object
   //return str.replace(regex, match=>escapeChars[match])
